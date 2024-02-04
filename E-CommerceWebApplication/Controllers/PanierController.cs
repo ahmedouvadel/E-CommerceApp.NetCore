5cfb66150680159e473a1d409f1b0594cec48f10
ahmedouvadel <ahmedvadel94@gmail.com>
2024-02-04 07:24:57 +0100

    07h:24

---
+++ E-CommerceWebApplication/Controllers/PanierController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_CommerceWebApplication.Controllers
+{
+    public class PanierController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
